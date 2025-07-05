70879ced2c33170553dd7066d6e1b225f9779f2e
ScarletLeaf1000 <ScarletLeaf1000@gmail.com>
2025-07-04 23:10:22 -0700

    worldgen setup

--- src/main/java/me/ScarletLeaf1000/arciniteMod/datagen/ModDatapackProvider.java
+++ src/main/java/me/ScarletLeaf1000/arciniteMod/datagen/ModDatapackProvider.java
@@ -3,11 +3,15 @@ package me.ScarletLeaf1000.arciniteMod.datagen;
 import me.ScarletLeaf1000.arciniteMod.ArciniteMod;
 import me.ScarletLeaf1000.arciniteMod.trim.ModTrimMaterials;
 import me.ScarletLeaf1000.arciniteMod.trim.ModTrimPatterns;
+import me.ScarletLeaf1000.arciniteMod.worldgen.ModBiomeModifiers;
+import me.ScarletLeaf1000.arciniteMod.worldgen.ModConfiguredFeatures;
+import me.ScarletLeaf1000.arciniteMod.worldgen.ModPlacedFeatures;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
@@ -15,7 +19,11 @@ import java.util.concurrent.CompletableFuture;
 public class ModDatapackProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
             .add(Registries.TRIM_MATERIAL, ModTrimMaterials::bootstrap)
-            .add(Registries.TRIM_PATTERN, ModTrimPatterns::bootstrap);
+            .add(Registries.TRIM_PATTERN, ModTrimPatterns::bootstrap)
+
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
 
     public ModDatapackProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(ArciniteMod.MOD_ID));
